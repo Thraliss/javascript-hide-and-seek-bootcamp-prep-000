64ac63c1f2f112c11d4754fc629a1faa34a237cd
Torren Martin <torren.martin89@gmail.com>
2018-01-13 18:24:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector){
+  return document.getElementById('app').querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n){
+  var lis = document.getElementById('app').querySelectorAll('.ranked-list');
+  for (var i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML + 1);
+  }
+  return lis;
+}
+
+function deepestChild(){
+  var lis = document.getElementById('grand-node').querySelectorAll('*');
+  
+  return lis[lis.length - 1];
+}
