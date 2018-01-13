a39b5799484892db4856888daf75ee6ed5e8eefb
Torren Martin <torren.martin89@gmail.com>
2018-01-13 18:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.getElementById('nested').querySelector('.target');
 }
+
+function increaseRankBy(n){
+  var lis = document.getElementById('app').querySelectorAll('.ranked-list');
+  for (var i = 0; i < lis.length; i++){
+    lis[i] = parseInt(i) + 1;
+  }
+  return lis;
+}
